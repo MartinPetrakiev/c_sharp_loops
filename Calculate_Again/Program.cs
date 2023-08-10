6e8578610c8fd1b85008cd05e81789389c39c640
martinmp <martomsl@gmail.com>
2023-08-10 13:18:25 +0300

    calculate again

---
+++ Calculate_Again/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace MyProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()!);
+            int k = int.Parse(Console.ReadLine()!);
+            int factN = 1;
+            int factK = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i <= k){
+                    factK *= i;
+                }
+                factN *= i;
+            }
+
+            Console.WriteLine(factN / factK);
+        }
+    }
+}
+
+/*
+Description
+Write a program that calculates N! / K! for given N and K.
+Use only one loop.
+
+Input
+On the first line, there will be only one number - N
+On the second line, there will be only one number - K
+
+
+Output
+Output a single line, consisting of the result from the calculation described above.
+
+Constraints
+1 < K < N < 100
+Hint: overflow is possible
+N and K will always be valid integer numbers
+Time limit: 0.1s
+Memory limit: 16MB
+*/
